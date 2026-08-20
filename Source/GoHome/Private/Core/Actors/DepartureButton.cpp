3a7c08d78219e30345985a2772c7a68f4e371a69
csk9914 <csk9914@gmail.com>
2026-08-20 15:34:02 +0900

    기능: 잠수정 귀환 시 문 닫힘 연출 대기 후 트래블
    ADepartureButton::OnInteract 귀환 분기:
    - DockingDoorComponent->SetOpen(false) 호출 직후 바로 트래블하던 것을,
    - FTimerHandle 기반 타이머(DoorCloseDelay)로 지연시켜 문 닫힘
    - DoorCloseDelay 기본값 8초 = Timeline 길이(5초) + 닫힌 상태를 보여주는 여유 시간 (3초)

--- Source/GoHome/Private/Core/Actors/DepartureButton.cpp
+++ Source/GoHome/Private/Core/Actors/DepartureButton.cpp
@@ -7,6 +7,7 @@
 #include "Core/GoHomeGameMode.h"
 #include "Core/LobbyGameState.h"
 #include "Core/DockingDoorComponent.h"
+#include "TimerManager.h"
 
 
 ADepartureButton::ADepartureButton()
@@ -23,25 +24,45 @@ void ADepartureButton::OnInteract(APawn* InstigatorPawn)
 	AGoHomeGameMode* GoHomeGameMode = GetWorld()->GetAuthGameMode<AGoHomeGameMode>();
 	if (!GoHomeGameMode) return;
 
-	FString TravelPath = "";
+
 
 	//  현재 로비 맵일 때
 	if (ALobbyGameState* LobbyGameState = GetWorld()->GetGameState<ALobbyGameState>())
 	{
-		// 탐사 맵으롷 이동
+		// 탐사 맵으롷 이동한다.
 		const UExpeditionZoneDataAsset* SelectedZone = LobbyGameState->GetSelectedZone();
 		if (!SelectedZone) return;
 
-		TravelPath = SelectedZone->MapPath.ToSoftObjectPath().GetLongPackageName();
+		GoHomeGameMode->ServerTravelViaLoadingScreen(SelectedZone->MapPath.ToSoftObjectPath().GetLongPackageName());
 	}
+	
+	// 현재 탐사 맵일 때
 	else if (AGoHomeGameState* GoHomeGameState = GetWorld()->GetGameState<AGoHomeGameState>())
 	{
+		// 문을 닫고
 		if (UDockingDoorComponent* DoorComponent = GoHomeGameState->GetDockingDoorComponent())
 		{
 			DoorComponent->SetOpen(false);
 		}
-		TravelPath = GoHomeGameMode->GetLobbyMapPath();
+		
+		// 타이머를 이용해서 문이 닫히는 시간까지 딜레이를 준다
+		// DoorCloseDelay 기다린 다음에 로비 맵으로 이동한다.
+		GetWorldTimerManager().SetTimer(
+			TravelDelayTimer, 
+			FTimerDelegate::CreateUObject(this, &ADepartureButton::ExecuteDelayedTravel, GoHomeGameMode->GetLobbyMapPath()),
+			DoorCloseDelay,
+			false);
+		
+		// CreateUObject를 쓰는 이유는 
+		// this(ADepartureButton 인스턴스)가 콜백 실행 전에 파괴돼도 
+		// 엔진이 안전하게 무시하기 때문(레벨 트래블 중 액터가 사라지는 경우)
 	}
+}
 
-	GoHomeGameMode->ServerTravelViaLoadingScreen(TravelPath);
+void ADepartureButton::ExecuteDelayedTravel(FString TravelPath)
+{
+	if (AGoHomeGameMode* GoHomeGameMode = GetWorld()->GetAuthGameMode<AGoHomeGameMode>())
+	{
+		GoHomeGameMode->ServerTravelViaLoadingScreen(TravelPath);
+	}
 }
--- Source/GoHome/Public/Core/Actors/DepartureButton.h
+++ Source/GoHome/Public/Core/Actors/DepartureButton.h
@@ -8,7 +8,7 @@
 #include "DepartureButton.generated.h"
 
 UCLASS()
-class GOHOME_API ADepartureButton : public AActor, public IInteractable    
+class GOHOME_API ADepartureButton : public AActor, public IInteractable
 {
 	GENERATED_BODY()
 
@@ -17,5 +17,17 @@ public:
 
 	virtual bool CanInteract(APawn* InstigatorPawn) const override;
 	virtual void OnInteract(APawn* InstigatorPawn) override;
+
+private:
+	void ExecuteDelayedTravel(FString TravelPath);
+	
+protected:
+	
+	// BP_Submarine의 Timeline_Door 길이(현재 5.0초), + 여유 값으로 3초 추가
+	UPROPERTY(EditDefaultsOnly, Category = "DockingDoor")
+	float DoorCloseDelay = 8.f;
+	
+private:
+	FTimerHandle TravelDelayTimer;
 	
 };
