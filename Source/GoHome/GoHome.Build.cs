fb48098677812a175f91df38ca7f80186ab4826e
csk9914 <csk9914@gmail.com>
2026-08-27 23:06:19 +0900

    기능: 보이스챗 입력 액션

--- Source/GoHome/GoHome.Build.cs
+++ Source/GoHome/GoHome.Build.cs
@@ -7,7 +7,7 @@ public class GoHome : ModuleRules
 	public GoHome(ReadOnlyTargetRules Target) : base(Target)
 	{
 		PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
-	
+
 		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "EnhancedInput", "Niagara" });
 
 		PrivateDependencyModuleNames.AddRange(new string[]
